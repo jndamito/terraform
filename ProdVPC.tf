642ec1c063934cf11d047da4e65073e74283579c
jndamito <josiasndam22@gmail.com>
2023-10-13 23:05:20 -0400

    tickets added

--- ProdVPC.tf
+++ ProdVPC.tf
@@ -109,4 +109,68 @@ resource "aws_route_table_association" "last_3_subnets_association" {
   route_table_id = aws_route_table.private_RT_prod2.id
 }
 
+/*
+# Transit gateway
 
+resource "aws_ec2_transit_gateway" "tgw" {
+  description = "TGW for prod and Bastion"
+}
+
+# Transit gateway attachement for Bastion
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_bastion" {
+  subnet_ids         = [aws_subnet.bastion-public.id]
+  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+  vpc_id             = aws_vpc.Bastion.id
+}
+
+# Transit gateway attachment for Prod
+resource "aws_ec2_transit_gateway_vpc_attachment" "tgw_prod" {
+  for_each = {
+    "subnet1" = aws_subnet.private_subnets[4].id,
+    "subnet2" = aws_subnet.private_subnets[5].id
+  }
+
+  subnet_ids         = [each.value]
+  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+  vpc_id            = aws_vpc.prodvpc.id
+
+}
+
+# Transit gateway route table
+
+resource "aws_ec2_transit_gateway_route_table" "tgw_rt" {
+  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+}
+
+# Transit gateway route table association and propagation for bastion_vpc
+resource "aws_ec2_transit_gateway_route_table_association" "tgw_rt_association" {
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.tgw_bastion.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_rt_propagation" {
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.tgw_bastion.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt.id
+}
+
+# Transit gateway route table association and propagation for prod_vpc
+resource "aws_ec2_transit_gateway_route_table_association" "tgw_rt_association2" {
+  for_each = {
+    "subnet4" = aws_subnet.private_subnets[4].id,
+    "subnet5" = aws_subnet.private_subnets[5].id
+  }
+
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.tgw_prod[each.value].id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt.id
+}
+
+resource "aws_ec2_transit_gateway_route_table_propagation" "tgw_rt_propagation2" {
+  for_each = {
+    "subnet4" = aws_subnet.private_subnets[4].id,
+    "subnet5" = aws_subnet.private_subnets[5].id
+  }
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_vpc_attachment.tgw_prod[each.value].id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.tgw_rt.id
+}
+*/
